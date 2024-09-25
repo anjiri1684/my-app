db4e70959ad225da4929ad792fa019e982fb3fab
vincent anjiri <vincentanjiri12@gmail.com>
2024-09-25 18:25:49 +0300

    firts commit

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const newYear = new Date().getFullYear();
+  const month = new Date().getMonth();
+  const date = new Date().getDate();
+
+  function digitalClock() {
+    const hour = new Date().getHours();
+    const minute = new Date().getMinutes();
+    const second = new Date().getSeconds();
+
+    return `${hour}:${minute}:${second}`;
+  }
+
+  setInterval(digitalClock, 1000);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>
+        {date}/{month}/{newYear}
+      </Text>
+      <Text>{digitalClock()}</Text>
+      <Text>Hello World!!</Text>
     </View>
   );
 }
@@ -13,8 +30,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
